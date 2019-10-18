7ecaff036e427b4de4e245b3cc5df0da284b4171
Reis Moritz (IFAT DCGR ATV PTS PMO / EE) <Moritz.Reis-EE@infineon.com>
2019-10-18 11:48:47 +0200

    add abstract class implementation

--- SVNPathCopy/SVNPathCopy.cs
+++ SVNPathCopy/SVNPathCopy.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SharpShell.SharpContextMenu;
+using System;
+using System.Windows.Forms;
 
 namespace SVNPathCopy
 {
-    public class SVNPathCopy
+    public class SVNPathCopy : SharpContextMenu
     {
+        protected override bool CanShowMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
