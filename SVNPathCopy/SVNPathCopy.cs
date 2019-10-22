0d4ff910bf0bb4dc78cb796f973849253fe0aaa2
Moritz Reis <clfaster@outlook.com>
2019-10-22 09:10:06 +0200

    add context menu item for copy svn path without revision

--- SVNPathCopy/SVNPathCopy.cs
+++ SVNPathCopy/SVNPathCopy.cs
@@ -1,37 +1,48 @@
 ﻿using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
 using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace SVNPathCopy
 {
     [ComVisible(true)]
-    [COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]
+    [COMServerAssociation(AssociationType.AllFiles)]
     public class SVNPathCopy : SharpContextMenu
     {
         protected override bool CanShowMenu()
         {
             // Todo: Should only be shown in SVN Repos!
-            return true;
+            // Can only be shown when one item is selected
+            return SelectedItemPaths.Count() == 1;
         }
 
         protected override ContextMenuStrip CreateMenu()
         {
             var menu = new ContextMenuStrip();
 
-            // Create Menu Entry
+            // Create Menu Entry with Revision
             var itemCopySVNPathWithRevision = new ToolStripMenuItem
             {
                 Text = "Copy SVN Path with Revision",
                 Image = Properties.Resources.share_svn
             };
 
+            // Create Menu Entry without Revision
+            var itemCopySVNPathWithoutRevision = new ToolStripMenuItem
+            {
+                Text = "Copy SVN Path",
+                Image = Properties.Resources.share_svn
+            };
+
             // Set action when clicked
             itemCopySVNPathWithRevision.Click += (sender, args) => CopySVNPath(true);
+            itemCopySVNPathWithoutRevision.Click += (sender, args) => CopySVNPath(false);
 
             // Add items to menu
             menu.Items.Add(itemCopySVNPathWithRevision);
+            menu.Items.Add(itemCopySVNPathWithoutRevision);
 
             return menu;
         }
