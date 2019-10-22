8154fda569026917b1d795e38d2852133a7336f4
Moritz Reis <clfaster@outlook.com>
2019-10-22 09:04:30 +0200

    add context menu item

--- SVNPathCopy/SVNPathCopy.cs
+++ SVNPathCopy/SVNPathCopy.cs
@@ -1,9 +1,13 @@
-﻿using SharpShell.SharpContextMenu;
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace SVNPathCopy
 {
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]
     public class SVNPathCopy : SharpContextMenu
     {
         protected override bool CanShowMenu()
@@ -16,12 +20,25 @@ namespace SVNPathCopy
         {
             var menu = new ContextMenuStrip();
 
+            // Create Menu Entry
             var itemCopySVNPathWithRevision = new ToolStripMenuItem
             {
                 Text = "Copy SVN Path with Revision",
                 Image = Properties.Resources.share_svn
             };
-            throw new NotImplementedException();
+
+            // Set action when clicked
+            itemCopySVNPathWithRevision.Click += (sender, args) => CopySVNPath(true);
+
+            // Add items to menu
+            menu.Items.Add(itemCopySVNPathWithRevision);
+
+            return menu;
+        }
+
+        private void CopySVNPath(bool with_revision)
+        {
+            MessageBox.Show("SVN Path with revision should be copied to clipboard");
         }
 
     }
